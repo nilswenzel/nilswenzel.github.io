93fba03fc276b2e91c5e412df52f7be32f26768e
nilswenzel <58890639+nilswenzel@users.noreply.github.com>
2022-10-11 00:50:54 +0200

    Add app layout

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
