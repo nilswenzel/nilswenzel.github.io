7cb8aa6f12430ed479ec589ca5d177a2c20a872b
nilswenzel <58890639+nilswenzel@users.noreply.github.com>
2023-02-10 17:53:57 +0100

    Add modal in timeline for more information

--- src/components/about/TimelineEducation.jsx
+++ src/components/about/TimelineEducation.jsx
@@ -9,11 +9,13 @@ const TimelineEducation = () => {
         <li className="mb-10 ml-4">
           <div className="absolute w-4 h-4 bg-gradient-to-r from-teal-500 to-teal-500 rounded-full mt-1.5 -left-2.5 border border-sky-50 dark:border-[#191a1f] dark:bg-teal-500"></div>
           <time className="flex items-center mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            <FaRegCalendarAlt className="h-3 w-3 mr-1" /> - April 2019
+            <FaRegCalendarAlt className="h-3 w-3 mr-1" /> April 2019
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Graduating from school (Abitur)
-          </h3>
+          <div className="flex items-center">
+            <span className="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              Graduating from school (Abitur)
+            </span>
+          </div>
           <p className="flex items-center mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             <IoLocationSharp className="h-4 w-4 mr-1" /> Waldschule
             Hagen-Beverstedt
@@ -24,9 +26,11 @@ const TimelineEducation = () => {
           <time className="flex items-center mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             <FaRegCalendarAlt className="h-3 w-3 mr-1" /> October 2019 -
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Starting university
-          </h3>
+          <div className="flex items-center">
+            <span className="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              Starting university
+            </span>
+          </div>
           <p className="flex items-center mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             <IoLocationSharp className="h-4 w-4 mr-1" /> Hochschule Bremerhaven
           </p>
--- src/components/about/TimelineWork.jsx
+++ src/components/about/TimelineWork.jsx
@@ -9,22 +9,34 @@ import { ReactComponent as GitLabIcon } from "../../assets/icons/gitlab.svg";
 import { ReactComponent as JiraIcon } from "../../assets/icons/jira.svg";
 import { ReactComponent as VueJSIcon } from "../../assets/icons/vuejs.svg";
 import { ReactComponent as JavaScriptIcon } from "../../assets/icons/javascript.svg";
-import { ReactComponent as HTMLIcon } from "../../assets/icons/html.svg";
-import { ReactComponent as CSSIcon } from "../../assets/icons/css.svg";
+
+import TimelineInfoModal from "./TimelineInfoModal";
 
 const TimelineWork = () => {
   return (
     <div>
       <ol className="relative z-0 border-l-4 border-teal-500 inline-block text-left">
-        <li className="mb-10 ml-4">
+        <li className="mb-10 ml-4 cursor-default">
           <div className="absolute w-4 h-4 bg-teal-500 rounded-full mt-1.5 -left-2.5 border border-sky-50 dark:border-[#191a1f] dark:bg-teal-500"></div>
           <time className="flex items-center mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
             <FaRegCalendarAlt className="h-3 w-3 mr-1" /> April 2022 - August
             2022
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-200">
-            Practical Semester/Internship
-          </h3>
+          <div className="flex items-center">
+            <span className="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              Practical Semester/Internship
+            </span>
+            <TimelineInfoModal
+              title="Practical Semester/Internship  (Software Engineer)"
+              location="Alfred Wegener Institut, Bremerhaven"
+              bulletpoints={[
+                "Setting up and managing a small baremetal Kubernetes cluster using Helm",
+                "Building CI/CD pipelines with GitLab & Kubernetes to automatically build packages, run tests, build Docker images and deploy review apps",
+                "Building Docker environments for developing",
+                "Using Jira as project management tool",
+              ]}
+            />
+          </div>
           <p className="flex items-center text-base font-normal text-gray-500 dark:text-gray-400">
             <IoLocationSharp className="h-4 w-4 mr-1" /> Alfred Wegener
             Institut, Bremerhaven
@@ -37,14 +49,27 @@ const TimelineWork = () => {
             <JiraIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
           </div>
         </li>
-        <li className="mb-10 ml-4">
+        <li className="mb-10 ml-4 cursor-default">
           <div className="absolute w-4 h-4 bg-teal-500 rounded-full mt-1.5 -left-2.5 border border-slate-50 dark:border-[#191a1f] dark:bg-teal-500"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
-            September 2022 -
+          <time className="flex items-center mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
+            <FaRegCalendarAlt className="h-3 w-3 mr-1" /> September 2022 -
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-200">
-            Software Engineer
-          </h3>
+          <div className="flex items-center">
+            <span className="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              Software Engineer
+            </span>
+            <TimelineInfoModal
+              title="Software Engineer"
+              location="Alfred Wegener Institut, Bremerhaven"
+              bulletpoints={[
+                "Setting up and managing a small baremetal Kubernetes cluster using Helm",
+                "Building CI/CD pipelines with GitLab & Kubernetes to automatically build packages, run tests, build Docker images and deploy review apps",
+                "Building Docker environments for developing",
+                "Building Vue.js components",
+                "Using Jira as project management tool",
+              ]}
+            />
+          </div>
           <p className="flex items-center text-base font-normal text-gray-500 dark:text-gray-400">
             <IoLocationSharp className="h-4 w-4 mr-1" /> Alfred Wegener
             Institut, Bremerhaven
@@ -57,8 +82,6 @@ const TimelineWork = () => {
             <JiraIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
             <VueJSIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
             <JavaScriptIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
-            <HTMLIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
-            <CSSIcon className="h-4 w-4 mr-2 fill-gray-500 dark:fill-gray-400" />
           </div>
         </li>
       </ol>
